e82b0bffe358697ea8f86fc9d94c4286f310c5c6
georgettenpm <georgette949@hotmail.es>
2023-03-23 12:42:53 +0100

    styling

---
+++ resources/views/activityForm.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <!-- Twitter meta-->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:site" content="@pratikborsadiya">
+    <meta property="twitter:creator" content="@pratikborsadiya">
+    <!-- Open Graph Meta-->
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Vali Admin">
+    <meta property="og:title" content="Vali - Free Bootstrap 4 admin theme">
+    <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
+    <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
+    <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <title>Form Samples - Vali Admin</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/linearicons-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+
+
+</head>
+</head>
+
+<body>
+<div class="p-b-63">
+							<a href="blog-detail.html" class="hov-img0 how-pos5-parent">
+								<img src="img/img-3.jpg" class="img-fluid w-50 h-50 mx-auto" alt="IMG-BLOG">
+
+								<div class="flex-col-c-m size-123 bg9 how-pos5">
+	
+								</div>
+							</a>
+
+							<div class="p-t-32">
+								<h4 class="p-b-15">
+									<a href="blog-detail.html" class="ltext-108 cl2 hov-cl1 trans-04 ">
+										CLASES DE PINTURA
+									</a>
+								</h4>
+
+								<p class="stext-117 cl6">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                 labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque. Sed ullamcorper
+                                  morbi tincidunt ornare massa eget egestas purus. Eu tincidunt tortor aliquam nulla facilisi cras 
+                                  fermentum odio eu. Vitae nunc sed velit dignissim sodales ut. Fames ac turpis egestas integer eget.
+                                   Non quam lacus suspendisse faucibus interdum posuere. Neque aliquam vestibulum morbi blandit cursus
+                                    risus at ultrices. Faucibus pulvinar elementum integer enim neque volutpat. Sit amet tellus cras adipiscing enim.
+                                 Sagittis orci a scelerisque purus.
+								</p>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="tile">
+                <h3 class="tile-title">FORMULARIO PARA PARTICIPAR EN LA ACTIVIDAD</h3>
+                <div class="tile-body">
+                    <form>
+                        <div class="form-group">
+                            <label class="control-label">Name</label>
+                            <input class="form-control" type="text" placeholder="Enter full name">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Email</label>
+                            <input class="form-control" type="email" placeholder="Enter email address">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Address</label>
+                            <textarea class="form-control" rows="4" placeholder="Enter your address"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Gender</label>
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="radio" name="gender">Male
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="radio" name="gender">Female
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Identity Proof</label>
+                            <input class="form-control" type="file">
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox">I accept the terms and conditions
+                                </label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
+
+</body>
+
+</html>
