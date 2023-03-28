1647ef8277abbeb048a98fd5c1888aa3d1d96512
georgettenpm <georgette949@hotmail.es>
2023-03-28 09:13:38 +0200

    activity view finished

--- resources/views/activityForm.blade.php
+++ resources/views/activityForm.blade.php
@@ -51,88 +51,78 @@
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 
-
-</head>
 </head>
 
 <body>
-<div class="p-b-63">
-							<a href="blog-detail.html" class="hov-img0 how-pos5-parent">
-								<img src="img/img-3.jpg" class="img-fluid w-50 h-50 mx-auto" alt="IMG-BLOG">
+<br/>
+    <br/>
+<div class="p-b-63 d-flex align-items-center">
+    
+    <a href="blog-detail.html" class="hov-img0 how-pos5-parent">
+        <img src="img/img-3.jpg" class="img-fluid w-60 h-60 mx-auto" alt="IMG-BLOG">
+        <div class="flex-col-c-m size-123 bg9 how-pos5"></div>
+        <h4 class="ltext-109 cl2 p-b-28 text-center">TALLER DE PINTURA</h4>
+    </a>
 
-								<div class="flex-col-c-m size-123 bg9 how-pos5">
-	
-								</div>
-							</a>
+    <div class="pl-3">
+        
+        <p class="stext-117 cl6 p-b-26">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet est vel orci luctus sollicitudin. Duis eleifend vestibulum justo, varius semper lacus condimentum dictum. Donec pulvinar a magna ut malesuada. In posuere felis diam, vel sodales metus accumsan in. Duis viverra dui eu pharetra pellentesque. Donec a eros leo. Quisque sed ligula vitae lorem efficitur faucibus. Praesent sit amet imperdiet ante. Nulla id tellus auctor, dictum libero a, malesuada nisi. Nulla in porta nibh, id vestibulum ipsum. Praesent dapibus tempus erat quis aliquet. Donec ac purus id sapien condimentum feugiat.</p>
+        <p class="stext-117 cl6 p-b-26">Praesent vel mi bibendum, finibus leo ac, condimentum arcu. Pellentesque sem ex, tristique sit amet suscipit in, mattis imperdiet enim. Integer tempus justo nec velit fringilla, eget eleifend neque blandit. Sed tempor magna sed congue auctor. Mauris eu turpis eget tortor ultricies elementum. Phasellus vel placerat orci, a venenatis justo. Phasellus faucibus venenatis nisl vitae vestibulum. Praesent id nibh arcu. Vivamus sagittis accumsan felis, quis vulputate.</p>
+    </div>
+</div>
 
-							<div class="p-t-32">
-								<h4 class="p-b-15">
-									<a href="blog-detail.html" class="ltext-108 cl2 hov-cl1 trans-04 ">
-										CLASES DE PINTURA
-									</a>
-								</h4>
 
-								<p class="stext-117 cl6">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                 labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque. Sed ullamcorper
-                                  morbi tincidunt ornare massa eget egestas purus. Eu tincidunt tortor aliquam nulla facilisi cras 
-                                  fermentum odio eu. Vitae nunc sed velit dignissim sodales ut. Fames ac turpis egestas integer eget.
-                                   Non quam lacus suspendisse faucibus interdum posuere. Neque aliquam vestibulum morbi blandit cursus
-                                    risus at ultrices. Faucibus pulvinar elementum integer enim neque volutpat. Sit amet tellus cras adipiscing enim.
-                                 Sagittis orci a scelerisque purus.
-								</p>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
 
-    <div class="row">
+    <br />
+    <br />
+
+    <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="tile">
-                <h3 class="tile-title">FORMULARIO PARA PARTICIPAR EN LA ACTIVIDAD</h3>
-                <div class="tile-body">
+                <h3 class="tile-title text-center">FORMULARIO PARA PARTICIPAR EN LA ACTIVIDAD</h3>
+                <div class="tile-body ">
                     <form>
                         <div class="form-group">
-                            <label class="control-label">Name</label>
-                            <input class="form-control" type="text" placeholder="Enter full name">
+                            <label class="control-label">Nombre</label>
+                            <input class="form-control" type="text" placeholder="nombre completo">
                         </div>
                         <div class="form-group">
-                            <label class="control-label">Email</label>
-                            <input class="form-control" type="email" placeholder="Enter email address">
+                            <label class="control-label">Correo electronico</label>
+                            <input class="form-control" type="email" placeholder="correo electronico">
                         </div>
                         <div class="form-group">
-                            <label class="control-label">Address</label>
-                            <textarea class="form-control" rows="4" placeholder="Enter your address"></textarea>
+                            <label class="control-label">Direccion</label>
+                            <textarea class="form-control" rows="4" placeholder="direccion"></textarea>
                         </div>
                         <div class="form-group">
-                            <label class="control-label">Gender</label>
+                            <label class="control-label">Genero</label>
                             <div class="form-check">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="gender">Male
+                                    <input class="form-check-input" type="radio" name="gender">Hombre
                                 </label>
                             </div>
                             <div class="form-check">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="gender">Female
+                                    <input class="form-check-input" type="radio" name="gender">Mujer
                                 </label>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label">Identity Proof</label>
+                            <label class="control-label">comprobacion de pago</label>
                             <input class="form-control" type="file">
                         </div>
                         <div class="form-group">
                             <div class="form-check">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" type="checkbox">I accept the terms and conditions
+                                    <input class="form-check-input" type="checkbox">acepto los terminos y condiciones
                                 </label>
                             </div>
                         </div>
                     </form>
                 </div>
-
+            
 
 
 </body>
